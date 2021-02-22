53ebef0cd8d4f626ee82f60beb8e138d80dcb508
minhnv1 <minhnvgch17079@fpt.edu.vn>
2021-02-22 22:41:53 +0700

    save

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -10,4 +10,12 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function responseToClient ($message, $success = false, $data = []) {
+        return json_encode([
+            'success' => $success,
+            'message' => $message,
+            'data'    => $data
+        ]);
+    }
 }
