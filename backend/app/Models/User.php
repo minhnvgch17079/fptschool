db21321ccb036f7357f953c2eb8a78a2287f8f7a
duong <duong@gmail.com>
2021-03-13 17:48:55 +0700

    fix all old code remaining and create edit function

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -6,7 +6,8 @@ class User extends \App\Models\BaseModel {
     protected $table = 'users';
 
     public function getDataByUsername ($username) {
-        $data = $this->model
+
+        $data = $this->model->table($this->table)
             ->where('username', '=', $username)
             ->first();
 
@@ -14,11 +15,11 @@ class User extends \App\Models\BaseModel {
     }
 
     public function insertData ($data) {
-        return $this->model->insert($data);
+        return  $this->model->table($this->table)->insert($data);
     }
 
     public function isExist ($username) {
-        $data = $this->model
+        $data = $this->model->table($this->table)
             ->where('username', '=', $username)
             ->first();
 
@@ -26,7 +27,7 @@ class User extends \App\Models\BaseModel {
     }
 
     public function getData ($username, $fullName, $email, $phone) {
-        $query = $this->model;
+        $query = $this->model->table($this->table);
 
         if (!empty($username))  $query->where('username', '=', $username);
         if (!empty($phone))     $query->where('phone_number', '=', $phone);
@@ -38,6 +39,26 @@ class User extends \App\Models\BaseModel {
         $data = $query->get([
             'id', 'username', 'full_name', 'phone_number', 'email', 'last_change_password', 'created', 'created_by',
             'modified', 'modified_by'
+        ]);
+
+        return json_decode(json_encode($data), true);
+    }
+
+    public function updateData ($username) {
+        $query = $this->model->table($this->table);
+
+        if (!empty($username))  $query->where('username', '=', $username);
+//        if (!empty($phone))     $query->where('phone_number', '=', $phone);
+//        if (!empty($fullName))  $query->where('full_name', 'like', "%$fullName%");
+//        if (!empty($email))     $query->where('email', '=', $email);
+
+        $query->limit(100);
+
+        $data = $query
+            ->update([
+                'username'
+//            , 'full_name', 'phone_number', 'email', 'last_change_password', 'created', 'created_by',
+//            'modified', 'modified_by'
         ]);
 
         return json_decode(json_encode($data), true);
