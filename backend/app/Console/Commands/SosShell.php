c1f343a2f67a4674ea1a669f1339f8e135c825f0
minhnv1 <minhnvgch17079@fpt.edu.vn>
2021-03-14 08:19:27 +0700

    refactor

--- backend/app/Console/Commands/SosShell.php
+++ backend/app/Console/Commands/SosShell.php
@@ -17,37 +17,4 @@ class SosShell extends BaseShell {
     protected $signature    = 'sos { function } {args?*}';
     protected $description  = 'Run each job with each worker';
 
-    public function calSalaryAllCodProvinceByDate () {
-        $date = $this->args[0] ?? null;
-
-        if (empty($date)) {
-            $this->out('Missing date');
-            return 1;
-        }
-
-        $this->ErpSgwCodSalaryFormula         = getInstance('ErpSgwCodSalaryFormula');
-        $this->TimeSheetsComponent            = new TimeSheetsComponent();
-        $this->ErpSalaryCodProvincesComponent = new ErpSalaryCodProvinceComponent();
-
-        $salaryMonth        = $this->TimeSheetsComponent->getSalaryMonthByDate($date);
-        $listCodProvince    = $this->ErpSgwCodSalaryFormula->getListCodsByMonthSalary($salaryMonth['month'], $salaryMonth['year'], 1000);
-
-        if (empty($listCodProvince)) {
-            $this->out('No list cod province founded');
-            return 1;
-        }
-
-        $isSuccess = 0;
-        $c = 0; $cc = count($listCodProvince);
-        foreach ($listCodProvince as $cod) {
-            $this->out(++$c . " / " . $cc);
-            $result = $this->ErpSalaryCodProvincesComponent->calSalForEachCodProvinceByDate($cod['cod_id'], $date, $date, 1000);
-            if (empty($result)) $isSuccess = 1;
-        }
-        return $isSuccess;
-    }
-
-    public function test () {
-        pd(getenv('BIG_DATA_HOST'));
-    }
 }
