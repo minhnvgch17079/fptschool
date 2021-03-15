4ad057e8e038f513747c2aaf6d0129686ffece69
minhnv1 <minhnvgch17079@fpt.edu.vn>
2021-03-16 01:14:54 +0700

    save file service feature

---
+++ backend/app/Models/FileUpload.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+class FileUpload extends BaseModel
+{
+    protected $table = 'files_upload';
+
+    public function save ($dataSave) {
+        return $this->model->table($this->table)
+            ->insert($dataSave);
+    }
+}
