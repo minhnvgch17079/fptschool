ed1ea54815b465a40a3b49cdc9dbdf283662cb50
minhnv1 <minhnvgch17079@fpt.edu.vn>
2021-03-16 01:28:49 +0700

    Validate file

--- backend/app/Http/Services/UploadFile.php
+++ backend/app/Http/Services/UploadFile.php
@@ -3,6 +3,7 @@
 namespace App\Http\Services;
 
 use App\Models\FileUpload;
+use function GuzzleHttp\Psr7\str;
 
 /**
  * Class UploadFile
@@ -11,12 +12,25 @@ use App\Models\FileUpload;
  */
 
 class UploadFile {
+    private $ruleNameLength = 200;
+    private $ruleFileSize   = 5228618;
 
     public function uploadSingleFile ($file, $fileName = null) {
         $fileNameDisplay = $fileName ?? $file->getClientOriginalName();
         $endType         = explode('.', $fileNameDisplay);
         $fileNameSave    = randomString(50) . "." .last($endType);
         $pathSave        = base_path().'/public/files/';
+        $fileSize        = $file->getSize();
+
+        if (strlen($fileNameDisplay) > $this->ruleNameLength) {
+            logErr("Class: " . __CLASS__ . " function: " . __FUNCTION__ . " line: " . __LINE__ . " error: File Name $fileNameDisplay must small than 200 characters" );
+            return false;
+        }
+
+        if ($fileSize > $this->ruleFileSize) {
+            logErr("Class: " . __CLASS__ . " function: " . __FUNCTION__ . " line: " . __LINE__ . " error: File to large, must smaller than 4,5 mb" );
+            return false;
+        }
 
         $this->FileUpload = getInstance('FileUpload');
 
@@ -34,7 +48,7 @@ class UploadFile {
             if ($result) return true;
             return false;
         } catch (\Exception $e) {
-            logErr($e->getMessage());
+            logErr("Class: " . __CLASS__ . "-function: " . __FUNCTION__ . "-line: " . __LINE__ . "-error: " . $e->getMessage());
         }
     }
 }
