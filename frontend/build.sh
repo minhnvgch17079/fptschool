6b428f2d43e5a2ac6c613c4ea29932e87edd6cba
minhnv1 <minhnvgch17079@fpt.edu.vn>
2021-03-29 11:45:04 +0700

    Save

--- frontend/build.sh
+++ frontend/build.sh
@@ -12,28 +12,18 @@ npm run build
 echo 'done build front end. Start copy process'
 
 
-rm -rf $frontend_dir/../backend/public/css
-rm -rf $frontend_dir/../backend/public/fonts
-rm -rf $frontend_dir/../backend/public/img
-rm -rf $frontend_dir/../backend/public/js
-
-rm -f $frontend_dir/../backend/public/favicon.ico
-rm -f $frontend_dir/../backend/public/loader.css
-rm -f $frontend_dir/../backend/public/logo.png
-rm -f $frontend_dir/../backend/public/adm/index.html
-
+rm -rf $frontend_dir/../backend/public/adm
 mkdir $frontend_dir/../backend/public/adm
 
+cp -r dist/css/ $frontend_dir/../backend/public/adm/css
+cp -r dist/fonts/ $frontend_dir/../backend/public/adm/fonts
+cp -r dist/img/ $frontend_dir/../backend/public/adm/img
+cp -r dist/js/ $frontend_dir/../backend/public/adm/js
 
-cp -r dist/css/ $frontend_dir/../backend/public/css
-cp -r dist/fonts/ $frontend_dir/../backend/public/fonts
-cp -r dist/img/ $frontend_dir/../backend/public/img
-cp -r dist/js/ $frontend_dir/../backend/public/js
-
-cp dist/favicon.ico $frontend_dir/../backend/public/
-cp dist/loader.css $frontend_dir/../backend/public/
-cp dist/logo.png $frontend_dir/../backend/public/
-cp dist/index.html $frontend_dir/../backend/public/adm/
+cp dist/favicon.ico $frontend_dir/../backend/public/adm/
+cp dist/loader.css $frontend_dir/../backend/public/adm/
+cp dist/logo.png $frontend_dir/../backend/public/adm/
+cp dist/index.html $frontend_dir/../backend/public/adm
 
 echo 'done all'
 
