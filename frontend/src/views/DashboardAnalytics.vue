150c5edcccbf957dd5c61c661bc2c35be15f8931
minhnv1 <minhnvgch17079@fpt.edu.vn>
2021-03-30 18:20:54 +0700

    divide header

--- frontend/src/views/DashboardAnalytics.vue
+++ frontend/src/views/DashboardAnalytics.vue
@@ -1,64 +1,6 @@
 <template>
   <div class="container-fluid mt-3">
-    <div class="mb-10">
-      <b-navbar toggleable="lg" type="dark" variant="info">
-        <b-navbar-brand href="#">
-          <logo style="width: 30px; height: 30px"></logo>
-        </b-navbar-brand>
-
-        <b-collapse id="nav-collapse" is-nav class="ml-10">
-          <b-navbar-nav>
-            <b-nav-item>
-              <router-link to="/dashboard/analytics">Home</router-link>
-            </b-nav-item>
-
-            <b-nav-item>
-              <router-link to="/config/closures">Manage Closure</router-link>
-            </b-nav-item>
-
-            <b-nav-item>
-              <router-link to="/config/faculties">Manage Faculty</router-link>
-            </b-nav-item>
-
-            <b-nav-item>
-              <router-link to="/user/user-list">Manage User</router-link>
-            </b-nav-item>
-            <b-nav-item>
-              <router-link to="/admin/report-error">Manage Report</router-link>
-            </b-nav-item>
-          </b-navbar-nav>
-        </b-collapse>
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-        <b-collapse id="nav-collapse" is-nav>
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto">
-
-            <b-nav-item-dropdown right>
-              <!-- Using 'button-content' slot -->
-              <template #button-content>
-                <em>User</em>
-              </template>
-              <b-dropdown-item>
-                <img v-b-toggle.collapse-1 style="width: 50px; height: 50px; border-radius: 50%" :src="'/user/getAvatar'">
-              </b-dropdown-item>
-              <b-dropdown-item v-b-modal.profileEdit>Profile</b-dropdown-item>
-              <b-dropdown-item>
-                <b-btn variant="outline-success" @click="uploadAvatar()">
-                  Upload Avatar
-                </b-btn>
-              </b-dropdown-item>
-              <b-dropdown-item v-b-modal.changePass>
-                Change Password
-              </b-dropdown-item>
-              <b-dropdown-item>
-                <b-btn variant="outline-secondary" @click="logout()">Logout</b-btn>
-              </b-dropdown-item>
-            </b-nav-item-dropdown>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
-    </div>
+    <header-fptschool></header-fptschool>
     <b-row>
       <b-col md="6">
         <div>
@@ -126,6 +68,7 @@ import Logo from "@/layouts/components/Logo";
 import ProfileEdit from "@/views/components/student/ProfileEdit";
 import ChangePass from "@/views/components/student/ChangePassword";
 import UploadAvatar from "@/views/components/student/UploadAvatar";
+import HeaderFptschool from "@/views/Header";
 
 export default {
   data() {
@@ -202,6 +145,7 @@ export default {
     }
   },
   components: {
+    HeaderFptschool,
     UploadAvatar,
     ChangePass,
     ProfileEdit,
